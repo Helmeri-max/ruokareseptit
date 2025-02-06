c25b9a1325b9a53488ecfe3a86f0551d1608bf48
Samuli Mikola <samulimikola@gmail.com>
2025-02-06 18:51:18 +0200

    added input length validation to forms

--- app.py
+++ app.py
@@ -1,8 +1,3 @@
-# Käyttäjä pystyy luomaan tunnuksen ja kirjautumaan sisään sovellukseen.
-# Käyttäjä pystyy lisäämään, muokkaamaan ja poistamaan tietokohteita.
-# Käyttäjä näkee sovellukseen lisätyt tietokohteet.
-# Käyttäjä pystyy etsimään tietokohteita hakusanalla tai muulla perusteella.
-
 from flask import Flask
 from flask import render_template, request, redirect, session, abort
 import sqlite3
@@ -16,29 +11,30 @@ from werkzeug.security import generate_password_hash, check_password_hash
 app = Flask(__name__)
 app.secret_key = config.secret_key
 
+
 @app.route("/")
 def index():
     recipes = get_recipes()
     return render_template("index.html", recipes=recipes)
 
-# lomakkeet joilla käyttäjä syöttää tiedot
-# ohjaa sivulle /create
+
 @app.route("/register")
 def register():
     return render_template("register.html")
 
-# nappaa ja käsittelee tiedot /registerin lomakkeelta
-# ohjaa kirjautumissivulle jos tunnuksen luonti onnistuu, muuten etusivulle
+
 @app.route("/create", methods=["POST"])
 def create():
     username = request.form["username"]
     password1 = request.form["password1"]
     password2 = request.form["password2"]
 
+    if len(username) > 100 or not username or len(password1) > 100 or not password1:
+        abort(403)
+
     if password1 != password2:
         return "Virhe! Salasanat eivät täsmää! <a href='/register'>Yritä uudelleen<a>"
     try:
-        # tallenna tunnus tietokantaan
         password_hash = generate_password_hash(password1)
         sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
         db.execute(sql, params=[username, password_hash])
@@ -46,16 +42,18 @@ def create():
         return "Virhe! Tunnus on jo käytössä! <a href='/register'>Yritä uudelleen<a>"
     return redirect("/login")
 
+
 @app.route("/login")
 def login():
     return render_template("login.html")
 
-# nappaa kirjautumistiedot /loginilta, käsittelee ne
-# ohjaa etusivulle
+
 @app.route("/process_login", methods=["POST"])
 def process_login():
     username = request.form["username"]
     password = request.form["password"]
+    if len(username) > 100 or len(password) > 100:
+        abort(403)
 
     try:
         sql = "SELECT password_hash FROM users WHERE username = ?"
@@ -83,8 +81,6 @@ def add_recipe_page():
     return render_template("add_recipe.html")
 
 
-# nappaa uuden reseptin tiedot add_recipe-sivun lomakkeelta ja lisää ne tietokantaan
-# ohjaa reseptisivulle
 @app.route("/process_recipe", methods=["POST"])
 def process_recipe():
     require_login()
@@ -93,9 +89,14 @@ def process_recipe():
     instructions = request.form["instructions"]
     user_id = session["user_id"]
 
+    if len(title) > 100 or not title or len(ingredients) > 5000 \
+    or len(instructions) > 5000:
+        abort(403)
+
     recipe_id = add_recipe(title, ingredients, instructions, user_id)
     
     return redirect("/recipe/" + str(recipe_id))
+
 
 @app.route("/recipe/<int:recipe_id>")
 def show_recipe(recipe_id):
@@ -103,6 +104,7 @@ def show_recipe(recipe_id):
     if not recipe:
         abort(404)
     return render_template("recipe.html", recipe=recipe )
+
 
 @app.route("/edit_recipe/<int:recipe_id>", methods=["GET", "POST"])
 def edit_recipe(recipe_id):
@@ -118,8 +120,11 @@ def edit_recipe(recipe_id):
     if request.method == "POST":
         ingredients = request.form["ingredients"]
         instructions = request.form["instructions"]
+        if len(ingredients) > 5000 or len(instructions) > 5000:
+            abort(403)
         update_recipe(recipe_id, ingredients, instructions)
         return redirect("/recipe/" + str(recipe_id))
+
 
 @app.route("/delete_recipe/<int:recipe_id>", methods=["GET", "POST"])
 def delete_recipe(recipe_id):
@@ -141,5 +146,7 @@ def delete_recipe(recipe_id):
 @app.route("/search")
 def search_page():
     query = request.args.get("query")
+    if len(query) > 100:
+        abort(403)
     results = search(query) if query else []
     return render_template("search.html", query=query, results=results)
